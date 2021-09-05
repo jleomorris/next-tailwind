e466bc054ac48fe32fc7e47149876c97b1e4f876
jleomorris <james-morris2010@hotmail.co.uk>
2021-09-05 03:18:42 +0100

    Add new components to examples

--- pages/examples/index.js
+++ pages/examples/index.js
@@ -2,16 +2,28 @@ import ReturnHome from '../../components/ReturnHome';
 import SkillCard from '../../components/skill-cards/SkillCard';
 import SkillCard2 from '../../components/skill-cards/SkillCard2.js';
 import SkillCard3 from '../../components/skill-cards/SkillCard3.js';
+import SkillCardTemplate from '../../components/skill-cards/SkillCardTemplate';
+import NewMessage from '../../components/ChitChat/NewMessage';
+import JobCard1 from '../../components/Job-cards/JobCard1';
 
 const examples = () => {
   return (
     <div className='p-20 bg-white'>
-      <h1 className='text-8xl mb-10'>Example components</h1>
-      <h2 className='mt-20 mb-10 text-4xl'>Skill Cards</h2>
-      <div className='space-x-4 flex flex-wrap'>
+      <h1 className='text-6xl mb-10'>Example components</h1>
+      <h2 className='my-10 text-4xl'>Skill Cards</h2>
+      <div className='flex flex-wrap justify-center items-center'>
         <SkillCard />
         <SkillCard2 />
         <SkillCard3 />
+        {/* <SkillCardTemplate /> */}
+      </div>
+      <h2 className='my-10 text-4xl'>Social App Cards</h2>
+      <div className='flex flex-wrap justify-center items-center'>
+        <NewMessage />
+      </div>
+      <h2 className='my-10 text-4xl'>Job Cards</h2>
+      <div className='flex flex-wrap justify-center items-center'>
+        <JobCard1 />
       </div>
       <ReturnHome />
     </div>
