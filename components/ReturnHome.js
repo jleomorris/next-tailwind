8b95bdd4c08256db892efb709bebe1cc56369cf9
jleomorris <james-morris2010@hotmail.co.uk>
2021-09-05 03:17:56 +0100

    Update return home btn styling

--- components/ReturnHome.js
+++ components/ReturnHome.js
@@ -1,6 +1,9 @@
 const ReturnHome = () => {
   return (
-    <a href='/' className='text-2xl block my-10'>
+    <a
+      href='/'
+      className='text-2xl block my-10 border-2 border-gray-500 hover:border-gray-600 text-gray-500 hover:text-gray-600 text-center rounded-xl  py-3 max-w-xs'
+    >
       <span className='pr-2'>&#8592;</span>Return to home
     </a>
   );
