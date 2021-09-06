c57ebff72b4cbbdb20a36d1eccfd627cfecbd672
jleomorris <james-morris2010@hotmail.co.uk>
2021-09-06 01:59:54 +0100

    Add Whatsapp notification and marketing card components

---
+++ components/Whatsapp/MessageNotification.js
@@ -0,0 +1,20 @@
+import Whatsapp from '../Icons/Whatsapp';
+
+const MessageNotification = () => {
+  return (
+    <div className='rounded-xl bg-white px-5 py-2 w-full md:w-3/4 lg:w-1/3 shadow-md'>
+      <div className='flex justify-between'>
+        <div className='flex'>
+          <div className='bg-green-400 rounded-md p-1'>
+            <Whatsapp />
+          </div>
+          <p className='ml-3 my-auto'>WHATSAPP</p>
+        </div>
+        <p>13m ago</p>
+      </div>
+      <p className='mt-2 font-bold'>Message</p>
+    </div>
+  );
+};
+
+export default MessageNotification;
